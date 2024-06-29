41045ac764adf1e80d15cfe02b3f5cadd8a1db38
VuVanNghia <vuvannghia452002@gmail.com>
2024-06-29 09:07:47 +0700

    update 4 files and create 1 file

--- code/main.py
+++ code/main.py
@@ -3,8 +3,8 @@ from modules.new_repo import NewRepo
 
 if __name__ == "__main__":
 
-    # root_folder = input(f"Nhập vị trí repo mới: ")
-    root_folder = r"c:\Users\vvn20206205\Desktop\test_rr"
+    # root_folder = r"c:\Users\vvn20206205\Desktop\test_rr"
+    root_folder = input(f"Nhập vị trí repo mới: ")
 
     new = NewRepo(root_folder)
     new.start()
--- code/modules/new_repo.py
+++ code/modules/new_repo.py
@@ -1,5 +1,6 @@
 import os
 from modules.create_file import CreateFile
+from modules.create_folder import CreateFolder
 
 
 class NewRepo:
@@ -7,30 +8,19 @@ class NewRepo:
         self.root_folder = root_folder
 
     def start(self):
+        contents = ""
         print(f"Thư mục gốc: {self.root_folder}")
-
+        #
         CreateFile(f"{self.root_folder}/README.md")
+        #
         CreateFile(f"{self.root_folder}/.gitignore")
-        contents = """
-
-{
-  "extensions": {
-    "recommendations": []
-  },
-  "folders": [
-    {
-      "path": "."
-    },
-    {
-      "path": "contents"
-    }
-  ],
-  "settings": {}
-}
-
-  
-
-
-
-"""
-        CreateFile(f"{self.root_folder}/{os.path.basename(self.root_folder)}.code-workspace")
+        #
+        with open(f"../nghia-github-template.code-workspace", "r", encoding="utf-8") as file:
+            contents = file.read()
+        CreateFile(f"{self.root_folder}/{os.path.basename(self.root_folder)}.code-workspace", contents)
+        #
+        CreateFolder(f"{self.root_folder}/contents")
+        #
+        with open(f"../contents/.gitignore", "r", encoding="utf-8") as file:
+            contents = file.read()
+        CreateFile(f"{self.root_folder}/contents/.gitignore", contents)
